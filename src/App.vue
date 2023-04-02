8989eb937761f610dc0a793e31df4e3badae4bb5
Maniple kwizera <maniple.kwizera@codeland.it>
2023-04-02 17:48:29 +0200

    using interface with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,28 @@
 <template>
  <div class="app">
- <p>{{ name }}-{{ age }}</p>
-  <button @click="changeName('Manzi')">Change name </button>
-  <button @click="changeAge('109')">Change age </button>
+  {{ jobs }}
  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job';
 export default defineComponent({
   name: 'App',
   components: { },
-  // data (){
-  //   return {
-  //     name: "Link",
-  //     age: 30 as number | string
-  //   }
-  // },
+ 
 
-  setup(){ // for composition api we introduce setup and we either use reactive or refs
-
-    // for reactive
-  //   const state = reactive({
-  //   name : 'LINK',
-  //   age: 10 as string | number
-  // })
-  // return{...toRefs(state)}
-
-  // for reference
-const name = ref('LINK')
-const age = ref<string | number>(40)
-return { name , age}
-  
+  setup(){
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+    return { jobs}
   },
-  methods:{
-    changeName (name : string){
-      this.name = name
-      return name
-    },
-    changeAge(age: number | string){
-      this.age = age
-      return age
-    }
-  }
+
 });
 </script>
 
